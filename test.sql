ced9517bbd44ba11fb703ce22fae0f414aeb7626
alecaballero1 <143503330+alecaballero1@users.noreply.github.com>
2023-12-18 15:08:53 +0100

    test file

---
+++ test.sql
@@ -0,0 +1,16 @@
+/*
+-- Query: SELECT * FROM usuarios
+LIMIT 0, 1000
+
+-- Date: 2023-12-18 13:37
+*/
+INSERT INTO `` (`id`,`name`,`email`,`age`,`note_desserts`,`gender`) VALUES (1,'Juan Perez','juan@example.com',20,'Normally only eats vanilla or strawberry desserts','Male');
+INSERT INTO `` (`id`,`name`,`email`,`age`,`note_desserts`,`gender`) VALUES (2,'Maria Gomez','maria@example.com',28,'Likes pastries and donuts but hates sour','Female');
+INSERT INTO `` (`id`,`name`,`email`,`age`,`note_desserts`,`gender`) VALUES (3,'Gonzalo Caballero','gonza@example.com',35,'Fan of chocolate but hate desserts too sweet','Male');
+INSERT INTO `` (`id`,`name`,`email`,`age`,`note_desserts`,`gender`) VALUES (4,'Isabela Monroy','chabe@example.com',19,'Likes fruity flavors, specially apple','Female');
+INSERT INTO `` (`id`,`name`,`email`,`age`,`note_desserts`,`gender`) VALUES (5,'Jorge Vargas','jorge@example.com',40,'Enjoys desserts with milk and cream cheese','Male');
+INSERT INTO `` (`id`,`name`,`email`,`age`,`note_desserts`,`gender`) VALUES (6,'Valeria Espinosa','vale@example.com',36,'Loves eating donuts. Hate sour flavors','Female');
+INSERT INTO `` (`id`,`name`,`email`,`age`,`note_desserts`,`gender`) VALUES (7,'Mario Ibireda','mario@example.com',21,'Only enjoys chocolate. Dislikes most flavors','Male');
+INSERT INTO `` (`id`,`name`,`email`,`age`,`note_desserts`,`gender`) VALUES (8,'Ale Monroy','ale@example.com',33,'Enjoys most desserts. Specially a fan of pastries','Female');
+INSERT INTO `` (`id`,`name`,`email`,`age`,`note_desserts`,`gender`) VALUES (9,'Carlos Perez','carlos@example.com',47,'Likes salty flavors. Also a fan of chocolate','Male');
+INSERT INTO `` (`id`,`name`,`email`,`age`,`note_desserts`,`gender`) VALUES (10,'Memo Orozco','memo@example.com',23,'Diabetic, can only eat low in sugar','Male');
