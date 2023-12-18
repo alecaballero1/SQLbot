0caf2c612673e5658c9d4e0f1bb471efc00048b0
alecaballero1 <143503330+alecaballero1@users.noreply.github.com>
2023-12-18 17:55:56 +0100

    Update web.py

--- web.py
+++ web.py
@@ -1,7 +1,22 @@
 # main_streamlit_script.py
 
+#libraries needed
 import streamlit as st
-from chat_bot import ChatBot
+from langchain.sql_database import SQLDatabase
+from langchain.llms import OpenAI
+from langchain_experimental.sql import SQLDatabaseChain
+from langchain.prompts import PromptTemplate
+from langchain.prompts.chat import HumanMessagePromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage, SystemMessage
+from langchain.prompts import PromptTemplate
+from langchain.agents.agent_toolkits import SQLDatabaseToolkit
+from langchain.memory import ConversationBufferMemory
+from langchain.agents.agent_types import AgentType
+from langchain.agents import create_sql_agent
+from openai import OpenAI
+import streamlit as st
+import pandas as pd
 
 def main():
     st.set_page_config(page_title="Chat with your sql!", page_icon=":cake:")
@@ -16,7 +31,7 @@ def main():
 
     # Inicia el chat bot
     api_key = st.secrets["OPENAI_API_KEY"]
-    chat_bot = ChatBot(api_key=api_key, sql_file=uploaded_file)
+    
 
     # Lógica de interacción con el bot
     if user_input:
