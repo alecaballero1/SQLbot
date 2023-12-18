75209d4ecfdde181b796eb2df18e30522e788fca
alecaballero1 <143503330+alecaballero1@users.noreply.github.com>
2023-12-18 17:57:01 +0100

    Update web.py

--- web.py
+++ web.py
@@ -18,6 +18,42 @@ from openai import OpenAI
 import streamlit as st
 import pandas as pd
 
+class ChatBot:
+    def __init__(self, api_key, use_database=False, db_uri=None, sql_file=None):
+        self.client = OpenAI(api_key=api_key)
+        llm_name = "gpt-3.5-turbo"
+        self.llm = ChatOpenAI(api_key=api_key, model_name=llm_name, temperature=0)
+        
+        if use_database and db_uri:
+            self.db = SQLDatabase.from_uri(db_uri)
+            self.db_chain = SQLDatabaseChain(llm=self.llm, database=self.db, verbose=True)
+        elif sql_file:
+            self.db = self.load_sql_file(sql_file)
+            self.db_chain = SQLDatabaseChain(llm=self.llm, database=self.db, verbose=True)
+        else:
+            self.db = None
+            self.db_chain = None
+
+    #load sql file
+    def load_sql_file(self, sql_file):
+        df = pd.read_sql(sql_file, con=self.db.engine)
+        db = SQLDatabase(data=df)
+        return db
+    
+    def retrieve_context(self, query):
+        db_context = self.db_chain(query)
+        return db_context['result'].strip()
+
+    def get_completion(self, prompt, model="gpt-3.5-turbo"):
+        messages = [{"role": "user", "content": prompt}]
+        response = self.client.chat.completions.create(
+            model=model,
+            messages=messages,
+            max_tokens=50,
+            temperature=0
+        )
+        return response.choices[0].message.content
+        
 def main():
     st.set_page_config(page_title="Chat with your sql!", page_icon=":cake:")
 
@@ -31,14 +67,13 @@ def main():
 
     # Inicia el chat bot
     api_key = st.secrets["OPENAI_API_KEY"]
-    
 
     # Lógica de interacción con el bot
     if user_input:
         st.write("Bot: Welcome, ask questions right away! Type 'exit' to end the chat.")
         convo = []
         
-        context_m = chat_bot.retrieve_context(user_input)
+        context_m = ChatBot.retrieve_context(user_input)
 
         # Prompt and user message 
         prompt = f"""You are a data analysis specialist checking a SQL file.
