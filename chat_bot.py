820a581b3d3b1ee203a9b1e3003f40eb6e07e7b2
alecaballero1 <143503330+alecaballero1@users.noreply.github.com>
2023-12-19 03:31:30 +0100

    Update chat_bot.py

--- chat_bot.py
+++ chat_bot.py
@@ -1,4 +1,4 @@
-# chat_bot.py
+# Imports
 import os
 from langchain.sql_database import SQLDatabase
 from langchain.llms import OpenAI
@@ -14,7 +14,7 @@ import streamlit as st
 import pandas as pd
 import pymysql
 
-# variables
+# Variables
 host = st.secrets["DB_HOST"]
 username = st.secrets["DB_USERNAME"]
 password = st.secrets["DB_PASSWORD"]
@@ -22,11 +22,11 @@ db = st.secrets["DB_NAME"]
 port = st.secrets["DB_PORT"]
 api_key = st.secrets["OPENAI_API_KEY"]
 mysql_uri = f"mysql+pymysql://{username}:{password}@{host}:{port}/{db}"
+llm_name = "gpt-3.5-turbo"
 
 class ChatBot:
     def __init__(self):
         self.client = OpenAI(api_key=api_key)
-        llm_name = "gpt-3.5-turbo"
         self.llm = ChatOpenAI(api_key=api_key, model_name=llm_name, temperature=0)
         self.db = SQLDatabase.from_uri(mysql_uri)
         self.db_chain = SQLDatabaseChain(llm=self.llm, database=self.db, verbose=True)
